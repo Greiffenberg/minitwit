bdefbe899d1676e5a3ed578b7419480b0a476446
Ask Greiffenberg <askg@itu.dk>
2020-02-12 20:18:11 +0100

    MongoDB connect local or remote set from globals

--- backend/src/models/connect_to_mongodb.js
+++ backend/src/models/connect_to_mongodb.js
@@ -1,14 +1,21 @@
 const globals = require('../config/globals.json');
 const mongoose = require('mongoose');
 
+if(globals.db_use_mongodb != "true") {
+    console.log('DB Not using MongoDB');
+    return;
+}
+
+const db_path = globals.db_use_local == 'true' ? globals.db_mongo_local_ip : globals.db_mongo_remote_ip;
+
 // CONNECT TO DB
-console.log("log", globals.db_mongo_remote_ip);
+console.log("DB Connectiong to: ", db_path);
 
 //connecting remote mongodb database named test
-const mongoDB = globals.db_mongo_remote_ip;
+const mongoDB = db_path;
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 
 //testing connectivity
 mongoose.connection.once('connected', function() {
-    console.log("Database connected successfully")
+    console.log("DB connected successfully")
 });
