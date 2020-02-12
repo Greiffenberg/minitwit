2426a019545e97344b7c828047850fda56f65455
Mathias H. Nielsen <mathias.hoeyrup@gmail.com>
2020-02-12 09:51:23 +0100

    Added topNav component

--- frontend/minitwit/src/App.js
+++ frontend/minitwit/src/App.js
@@ -5,10 +5,12 @@ import { BrowserRouter as Router, Route} from 'react-router-dom';
 import Login from './pages/login/login';
 import Register from './pages/register/register';
 import Timeline from './pages/timeline/timeline';
+import TopNav from './components/topNav/topNav';
 
 function App() {
   return (
       <Router>
+          <TopNav/>
           <Route exact path = "/login" render={() => <Login/>} />
           <Route exact path = "/register" render={() => <Register/>} />
           <Route exact path = "/timeline" render={() => <Timeline/>} />
