69abc7626989a81273276246876865d4a68877e1
Mark Kragerup <markido@live.dk>
2020-02-16 16:19:04 +0100

    Update MERN_minitwit_sim_api_test.py

--- backend/tests/MERN_minitwit_sim_api_test.py
+++ backend/tests/MERN_minitwit_sim_api_test.py
@@ -16,11 +16,13 @@ HEADERS = {'Connection': 'close',
            f'Authorization': f'Basic {ENCODED_CREDENTIALS}'}
 
 def clear_test_db():
+    print("\n-------------------------------------")
     url = f"{BASE_URL}/clear_db"
     response = requests.get(url, headers=HEADERS)
     print("TEST DB CLEARED / INITIALIZED")
 
 def test_latest():
+    print("\n-------------------------------------")
     # post something to update LATEST
     print("TESTING: test_latest")
     url = f"{BASE_URL}/register"
@@ -36,10 +38,11 @@ def test_latest():
     response = requests.get(url, headers=HEADERS)
     assert response.ok
     assert response.json()['latest'] == 1337
-    print("PASSED: test_latest")
+    print(">PASSED: test_latest")
 
 
 def test_register():
+    print("\n-------------------------------------")
     print("TESTING: test_register")
     username = 'a'
     email = 'a@a.a'
@@ -54,10 +57,11 @@ def test_register():
     # verify that latest was updated
     response = requests.get(f'{BASE_URL}/latest', headers=HEADERS)
     assert response.json()['latest'] == 1
-    print("PASSED: test_register")
+    print(">PASSED: test_register")
 
 
 def test_create_msg():
+    print("\n-------------------------------------")
     print("TESTING: test_create_msg")
     username = 'a'
     data = {'content': 'Blub!'}
@@ -70,10 +74,11 @@ def test_create_msg():
     # verify that latest was updated
     response = requests.get(f'{BASE_URL}/latest', headers=HEADERS)
     assert response.json()['latest'] == 2
-    print("PASSED: test_create_msg")
+    print(">PASSED: test_create_msg")
 
 
 def test_get_latest_user_msgs():
+    print("\n-------------------------------------")
     print("TESTING: test_get_latest_user_msgs")
     username = 'a'
 
@@ -92,10 +97,11 @@ def test_get_latest_user_msgs():
     # verify that latest was updated
     response = requests.get(f'{BASE_URL}/latest', headers=HEADERS)
     assert response.json()['latest'] == 3
-    print("PASSED: test_get_latest_user_msgs")
+    print("PASSE D: test_get_latest_user_msgs")
 
 
 def test_get_latest_msgs():
+    print("\n-------------------------------------")
     print("TESTING: test_get_latest_msgs")
     username = 'a'
     query = {'no': 20, 'latest': 4}
@@ -113,10 +119,11 @@ def test_get_latest_msgs():
     # verify that latest was updated
     response = requests.get(f'{BASE_URL}/latest', headers=HEADERS)
     assert response.json()['latest'] == 4
-    print("PASSED: test_get_latest_msgs")
+    print(">PASSED: test_get_latest_msgs")
 
 
 def test_register_b():
+    print("\n-------------------------------------")
     print("TESTING: test_register_b")
     username = 'b'
     email = 'b@b.b'
@@ -131,10 +138,11 @@ def test_register_b():
     # verify that latest was updated
     response = requests.get(f'{BASE_URL}/latest', headers=HEADERS)
     assert response.json()['latest'] == 5
-    print("PASSED: test_register_b")
+    print(">PASSED: test_register_b")
 
 
 def test_register_c():
+    print("\n-------------------------------------")
     print("TESTING: test_register_c")
     username = 'c'
     email = 'c@c.c'
@@ -148,10 +156,11 @@ def test_register_c():
     # verify that latest was updated
     response = requests.get(f'{BASE_URL}/latest', headers=HEADERS)
     assert response.json()['latest'] == 6
-    print("PASSED: test_register_c")
+    print(">PASSED: test_register_c")
 
 
 def test_follow_user():
+    print("\n-------------------------------------")
     print("TESTING: test_follow_user")
     username = 'a'
     url = f'{BASE_URL}/fllws/{username}'
@@ -178,10 +187,11 @@ def test_follow_user():
     # verify that latest was updated
     response = requests.get(f'{BASE_URL}/latest', headers=HEADERS)
     assert response.json()['latest'] == 9
-    print("PASSED: test_follow_user")
+    print(">PASSED: test_follow_user")
 
 
 def test_a_unfollows_b():
+    print("\n-------------------------------------")
     print("TESTING: test_a_unfollows_b")
     username = 'a'
     url = f'{BASE_URL}/fllws/{username}'
@@ -202,18 +212,35 @@ def test_a_unfollows_b():
     # verify that latest was updated
     response = requests.get(f'{BASE_URL}/latest', headers=HEADERS)
     assert response.json()['latest'] == 11
-    print("PASSED: test_a_unfollows_b")
+    print(">PASSED: test_a_unfollows_b")
 
 # Init DB and run the tests
+no_of_test = "9"
 clear_test_db()
 
 test_latest()
+print("Passed tests: 1 / " + no_of_test)
+
 test_register()
+print("Passed tests: 2 / " + no_of_test)
+
 test_create_msg()
+print("Passed tests: 3 / " + no_of_test)
+
 test_get_latest_user_msgs()
+print("Passed tests: 4 / " + no_of_test)
+
 test_get_latest_msgs()
+print("Passed tests: 5 / " + no_of_test)
 
 test_register_b()
+print("Passed tests: 6 / " + no_of_test)
+
 test_register_c()
+print("Passed tests: 7 / " + no_of_test)
+
 test_follow_user()
+print("Passed tests: 8 / " + no_of_test)
+
 test_a_unfollows_b()
+print("Passed tests: 9 / " + no_of_test)
