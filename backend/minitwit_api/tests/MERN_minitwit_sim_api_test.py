ddabaf41c3d5c59d85c1920d063b55693a1b2cd0
Mark Kragerup <markido@live.dk>
2020-02-17 23:45:05 +0100

    Update MERN_minitwit_sim_api_test.py

--- backend/minitwit_api/tests/MERN_minitwit_sim_api_test.py
+++ backend/minitwit_api/tests/MERN_minitwit_sim_api_test.py
@@ -5,8 +5,8 @@ import sqlite3
 import requests
 from contextlib import closing
 
-#BASE_URL = 'http://104.248.246.24'
-BASE_URL = 'http://127.0.0.1:3005'
+BASE_URL = 'http://104.248.246.24'
+#BASE_URL = 'http://127.0.0.1:3005'
 
 USERNAME = 'simulator'
 PWD = 'super_safe!'
