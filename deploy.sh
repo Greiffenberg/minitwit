d784a9545ee12e2a6510e8500513f0efbde24b61
Ask Greiffenberg <askg@itu.dk>
2020-03-16 12:52:35 +0100

    Deploy script runs lint-project.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 git remote add release https://github.com/DevOpsITU/minitwit.git
 
@@ -7,5 +7,21 @@ git stash
 git checkout master
 git pull
 
+cd tests
+./lint-project.sh
+lintexitcode=$?
+cd ..
+
+if [ $lintexitcode != 0 ]
+then
+  echo -e "\e[31mLinting errors on master. Fix and try again\e[0m"
+  echo -e "\e[31mGit stays on master branch\e[0m"
+  exit 1
+fi
+
 
 git push release master
+
+Switch back to previous branch and "un-stash"
+git checkout @{-1}
+git stash apply
