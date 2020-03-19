45a48697314f556d537811351635176a683dfc1e
Ask Greiffenberg <askg@itu.dk>
2020-03-19 15:45:42 +0100

    Renamed folder monitor -> mon-log

--- docker-build-local-images.sh
+++ docker-build-local-images.sh
@@ -2,3 +2,5 @@ docker build -t onedevopsitu/frontend:latest . -f frontend/Dockerfile.dev
 docker build -t onedevopsitu/backend:latest . -f backend/Dockerfile.be
 docker build -t onedevopsitu/db:latest . -f backend/Dockerfile.db
 docker build -t onedevopsitu/test:latest . -f tests/Dockerfile.test
+docker build -t onedevopsitu/grafana:latest . -f mon-log/grafana/Dockerfile
+docker build -t onedevopsitu/prometheus:latest . -f mon-log/prometheus/Dockerfile
